5b21e9487a550e0c801e4b4e4787ce8e0eed39cf
TheLazySol <tbelonog25@gmail.com>
2025-05-14 17:27:04 -0400

    Increment package version to 0.8.3 and update DAO page content for clarity and transparency
    - Updated the version in package.json to 0.8.3.
    - Revised the DAO page text to enhance clarity, including a new introduction section explaining the purpose and necessity of EpicentralDAO.
    - Adjusted proposal creation requirements and community feedback process for better understanding.

--- src/app/dao/page.tsx
+++ src/app/dao/page.tsx
@@ -28,7 +28,7 @@ export default function DAOPage() {
               </span>
             </h1>
             <p className="text-xl text-white/70">
-              A community-driven governance system for the future of DeFi on Solana
+              Transparent | Decentralized | Community-driven.
             </p>
             <div className="flex flex-col items-center gap-8">
               <div className="flex items-center justify-center gap-4">
@@ -108,9 +108,44 @@ export default function DAOPage() {
             What is a DAO?
           </h2>
           <p className="text-lg text-white/70">
-            A Decentralized Autonomous Organization (DAO) is a revolutionary form of governance that allows communities to make decisions collectively without centralized control. 
-            DAOs are important because they empower individuals to have a direct say in the direction and management of projects, ensuring transparency, inclusivity, and decentralization. 
-            By leveraging Solana&apos;s blockchain and goverance tools, EpicentralDAO provides a secure and efficient way to coordinate and manage resources, making them a vital component of the future of decentralized finance and beyond.
+            "Decentralized Autonomous Organization" <br />
+            Lives on the blockchain.
+          </p>
+        </div>
+      </section>
+
+      {/* New DAO Introduction Section */}
+      <section className="container mx-auto px-4 py-24">
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-3xl font-light text-white/90 mb-8">
+            What is a DAO used for?
+          </h2>
+          <p className="text-lg text-white/70">
+            <strong>DAOs are used to make financial and operational decisions</strong> -- much like a corporation. 
+            <br />
+            <br />
+            However, EpicentralDAO is unique in that <strong>anyone can propose what to do with the treasury</strong>.
+            <br />
+            <br />
+            For as long as you're a $LABS Token holder, you can vote to make decisions on proposals, transfer funds, update protocol parameters, and more.
+          </p>
+        </div>
+      </section>
+
+      {/* New DAO Introduction Section */}
+      <section className="container mx-auto px-4 py-24">
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-3xl font-light text-white/90 mb-8">
+            Why is EpicentralDAO needed?
+          </h2>
+          <p className="text-lg text-white/70">
+            Transparency is key to building trust.
+            <br />
+            <br />
+            <strong>EpicentralDAO is a way to make sure that the Epicentral protocol is always transparent, operated in the best interest of $LABS token holders as well as the rest of the Solana Ecosystem.</strong>
+            <br />
+            <br />
+            The <span className="text-[#4a85ff]">Core Team</span> is responsible for the day-to-day operations of the protocol, but the $LABS token holders are those who can make decisions on how to use funds, propose programmatic protocol changes, etc. 
           </p>
         </div>
       </section>
@@ -127,15 +162,15 @@ export default function DAOPage() {
             <div className="relative z-30 max-w-lg bg-black/30 backdrop-blur-md border border-white/10 rounded-xl p-8 transform hover:-translate-y-2 transition-transform duration-300">
               <h3 className="text-xl font-medium text-white/90 mb-2">1. Draft Proposal</h3>
               <p className="text-base text-white/70">
-                Any member with <strong>1,000</strong> $LABS can draft a proposal in our <a href="https://discord.gg/5asAuY2sR8" className="text-[#4a85ff] hover:text-[#4a85ff] hover:drop-shadow-[0_0_0.3rem_#4a85ff] transition-all duration-300">Discord</a>. Proposals can range from parameter adjustments to new feature implementations.
+                Any member with <u>100,000 $LABS</u> can create a proposal either in <a href="https://app.realms.today/dao/LABS" className="text-[#4a85ff] hover:text-[#4a85ff] hover:drop-shadow-[0_0_0.3rem_#4a85ff] transition-all duration-300">Realms</a> or in the Epicentral Labs <a href="https://discord.gg/5asAuY2sR8" className="text-[#4a85ff] hover:text-[#4a85ff] hover:drop-shadow-[0_0_0.3rem_#4a85ff] transition-all duration-300">Discord</a>.
               </p>
             </div>
 
             {/* Card 2 */}
             <div className="relative z-20 max-w-lg bg-black/30 backdrop-blur-md border border-white/10 rounded-xl p-8 -mt-4 ml-20 transform hover:-translate-y-2 transition-transform duration-300">
-              <h3 className="text-xl font-medium text-white/90 mb-2">2. Community Review & Feedback</h3>
+              <h3 className="text-xl font-medium text-white/90 mb-2">2. Community Feedback</h3>
               <p className="text-base text-white/70">
-                The community discusses the proposal, providing feedback and suggestions during a review period before offically submitting the proposal on-chain.
+                Whether created in Realms or Discord, the community will then discuss the proposal and provide feedback in designated channels.
               </p>
             </div>
 
@@ -143,7 +178,7 @@ export default function DAOPage() {
             <div className="relative z-10 max-w-lg bg-black/30 backdrop-blur-md border border-white/10 rounded-xl p-8 -mt-4 ml-40 transform hover:-translate-y-2 transition-transform duration-300">
               <h3 className="text-xl font-medium text-white/90 mb-2">3. Voting Period</h3>
               <p className="text-base text-white/70">
-                $LABS token holders can stake their tokens in the Epicentral DAO to vote on proposals, with voting power proportional to their holdings.
+                Holders that staked into the EpicentralDAO, use their $LABS to vote on live proposals -- with voting power proportional to their holdings.
               </p>
             </div>
 
@@ -151,7 +186,7 @@ export default function DAOPage() {
             <div className="relative max-w-lg bg-black/30 backdrop-blur-md border border-white/10 rounded-xl p-8 -mt-4 ml-60 transform hover:-translate-y-2 transition-transform duration-300">
               <h3 className="text-xl font-medium text-white/90 mb-2">4. Implementation</h3>
               <p className="text-base text-white/70">
-                If a proposal is approved, it is implemented then executed via on-chain instructions or a new feature is added to the protocol by the Core Team.
+                After the voting period (1-3 days), if a proposal is approved by the public, it is then implemented by the Core Team or automatically via an executable on-chain instruction.
               </p>
             </div>
           </div>
